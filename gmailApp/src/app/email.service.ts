f117d396f44e7b8f40bcd4cba928b0a6a29dc921
github_agulseren19 <agulseren19@ku.edu.tr>
2021-07-05 20:12:11 +0300

    got data from api using .service

--- gmailApp/src/app/email.service.ts
+++ gmailApp/src/app/email.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-import {Observable,of} from 'rxjs'
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Email } from 'Email';
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.getEmails();
+   }
+
+getEmails():Observable<Email[]>{
+  return this.http.get<Email[]>('http://localhost:4200/api/email');
+
+}
+getEmailsId(id:number):Observable<Email[]>{
+  return this.http.get<Email[]>('http://localhost:4200/api/email'+'/'+id+'/sent');
+
+}
 }
--- gmailApp/src/app/user.service.ts
+++ gmailApp/src/app/user.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Observable,of} from 'rxjs'
+import { MailUser } from 'MailUser';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  private messageSource=new BehaviorSubject<number>(0);
+  currentMessage=this.messageSource.asObservable();
+  constructor(private http:HttpClient) {
+    this.getUsers();
+    this.getUsersId(0);
+   }
+getUsers():Observable<MailUser[]>{
+  return this.http.get<MailUser[]>('http://localhost:4200/api/user');
+}
+getUsersId(id:number):Observable<MailUser>{
+  return this.http.get<MailUser>('http://localhost:4200/api/user'+'/'+id);
+}
+changeMessage(message:number){
+  this.messageSource.next(message);
+     }
 }
