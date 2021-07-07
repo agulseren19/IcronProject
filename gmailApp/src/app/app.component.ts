e1419d518ac31c8182a6d8817646f7a687eaaf26
github_agulseren19 <agulseren19@ku.edu.tr>
2021-07-07 20:54:34 +0300

    added routing and style

--- gmailApp/src/app/app.component.ts
+++ gmailApp/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {faEnvelope,faBars,faUserCircle,faInbox,faStar,faClock,faPaperPlane,faStickyNote } from '@fortawesome/free-solid-svg-icons';
+import { MailUser } from 'MailUser';
 import { UserComponent } from './user/user.component';
+//import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ /* ngOnInit(){
+    $(document).ready(function () {
+
+      $(".more").click(function () {
+          $(".appear").toggle("fast");
+      });
+  });
+  }*/
   title = 'gmailApp';
   faEnvelope = faEnvelope;
   faBars=faBars;
@@ -16,5 +26,4 @@ export class AppComponent {
   faClock=faClock;
   faPaperPlane=faPaperPlane;
   faStickyNote=faStickyNote;
-
 }
