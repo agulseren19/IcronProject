827ad3a000420aab8a7ded6ad682ed527bc3a168
github_agulseren19 <agulseren19@ku.edu.tr>
2021-07-07 20:55:27 +0300

    updated routing

--- gmailApp/src/app/app-routing.module.ts
+++ gmailApp/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './blank/blank.component';
+import { EmailComponent } from './email/email.component';
+import { SentEmailComponent } from './sent-email/sent-email.component';
+//import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  //{path:'',redirectTo:'/inbox',pathMatch:’full'},
-  //{path:'inbox',component:InboxComponent},
-  //{path:'sent',component:SentComponent}
+  //{path:'',redirectTo:'/'},
+  {path:'snoozed',component:BlankComponent},
+  {path:'drafts',component:BlankComponent},
+  //{path:'login',component:UserComponent},
+  {path:'inbox',component:EmailComponent},
+  {path:'sent',component:SentEmailComponent}
 ];
 
 @NgModule({
